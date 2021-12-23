80ded438837c3460e37ab970afc4cda2f8461549
Luan Saboia <luan.saboia@gmail.com>
2021-12-23 13:35:20 -0300

    Initial commit

---
+++ Course/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using Course.Entities;
+using Course.Entities.Enums;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = (OrderStatus)Enum.Parse(typeof(OrderStatus), "Delivered");
+            /*OrderStatus os;
+            Enum.TryParse("Delivered", out os);*/
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+        }
+    }
+}
